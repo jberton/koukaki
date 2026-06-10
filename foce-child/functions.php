abca52a730cb8f8fa50a96cc350c607f18a13513
Jeremie Berton <bertonjer@hotmail.com>
2026-06-11 00:31:54 +0200

    MAJ WP

---
+++ foce-child/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    // Chargement du fichier css du thème parent
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    // Chargement du fichier css du thème enfant pour nos personnalisations
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/style.css', array(), filemtime(get_stylesheet_directory() . '/css/style.css'));
+}
+
+// Get customizer options form parent theme
+if ( get_stylesheet() !== get_template() ) {
+    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
+        update_option( 'theme_mods_' . get_template(), $value );
+        return $old_value; // prevent update to child theme mods
+    }, 10, 2 );
+    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
+        return get_option( 'theme_mods_' . get_template(), $default );
+    } );
+}
+
+// Chargement des fichiers JS
+add_action( 'wp_enqueue_scripts', 'child_theme_javascript' );
+function child_theme_javascript(){
+    wp_enqueue_script( "swiper-js", "https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js", array(), false, true);
+    wp_enqueue_script( "skrollr-js", get_stylesheet_directory_uri() . "/assets/js/skrollr.min.js", array(), false, true);
+    wp_enqueue_script( "child-js", get_stylesheet_directory_uri() . "/assets/js/script.js", array(), false, true );
+}
+
+?>
---
+++ foce-child/template-parts/oscars.php
@@ -0,0 +1,6 @@
+<section id="oscars">
+    <div class="oscars-msg">
+        <h3>Fleurs d’oranger & chats errants<br> est nominé aux Oscars Short<br> Film Animated de 2022 !</h3>
+    </div>
+    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/courts-metrages-francais-d-animation-eligibles-aux-oscars-2021.png'; ?>" alt="courts metrages francais d'animation eligibles aux oscars 2021">
+</section>
